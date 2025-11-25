0f9eb9fb3e5ef098ec4d44b4a65227f07a2af481
NeuralCoder18 <neuralcipher1918@gmail.com>
2025-11-25 19:28:08 +0530

    Numpy Array Operations

---
+++ npyarray_operation.py
@@ -0,0 +1,21 @@
+import numpy as np
+a1=np.arange(12).reshape(3,4)
+a2=np.arange(12,24).reshape(3,4)
+
+#=============================
+#.   SCALAR OPERATION.    ||||
+#=============================
+
+#arithmetic
+#+,-,*,/,%,**(all in python)
+
+#relational(if satisfied true else false)
+#==,>=,<=,<,>,!=
+
+#==============================
+#.     VECTOR OPERATION.   ||||
+#==============================
+#arithmetic(shape should be same)
+#follows +,-,*,/,** with corresponding element of both a1 and a2
+
+
