cb022cb79338b07876b2eb44aabdc83c365e7546
NeuralCoder18 <neuralcipher1918@gmail.com>
2025-12-07 18:42:16 +0530

    Plotting Some Graph

---
+++ plotting_numpygraph.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#======================
+#.  Plotting a 2d graph ||
+#======================
+
+# #x=y
+# x=np.linspace(-10,10,100)
+# y=x
+# z=plt.plot(x,y)
+# print(z)
+
+# plt.show()
+
+# # y=x^2
+# a=np.linspace(-10,10,100)
+# b=a**2
+# plt.figure()
+# plt.plot(a,b)
+
+
+# plt.show()
+
+# ##y=sin(x)
+# d=np.linspace(-10,10,100)
+# e=np.sin(d)
+# plt.plot(d,e)
+# plt.show()
+
+# #y=xlog(x)
+# f=np.linspace(-10,10,100)
+# g=f*np.log(f)
+# plt.plot(f,g)
+# plt.show()
+
+
+
+
